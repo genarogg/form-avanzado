9df719ee832cfd04d63f77c1f9f64172996f3631
genarogg <genarrogg@gmail.com>
2021-07-14 00:10:30 -0400

    comfiguracion basica

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Form from "./pages/my-form/components/form-login"
 
+import "./css/icomoon/style.css"
 function App() {
   return (
-    <ProyectoState>
-      <TareaState>
+   
         <Router>
           <Switch>
-            
+          <Route exact path="/" component={Form} />
             {/* <Route exact path="/" component={Login} /> */}
             {/* <Route component={Proyectos} /> */}
           </Switch>
         </Router>
-      </TareaState>
-    </ProyectoState>
+   
   );
 }
 
