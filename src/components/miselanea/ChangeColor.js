2eee823bdb507c365cf9fbdd4b325f1f97da2b3f
genarogg <genarrogg@gmail.com>
2021-07-23 20:00:23 -0400

    funcion change

--- src/components/miselanea/ChangeColor.js
+++ src/components/miselanea/ChangeColor.js
@@ -1,42 +1,89 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { $classList, $toggle } from "../../functions/$";
 
 import "../../css/miselanea/changeColor.css";
 
 import Icono from "../nano/Icono";
 
 const ChangeColor = ({ position }) => {
-  const change = () => {};
+  const active = () => {
+    $toggle("changeColor", "active");
+  };
+
+  const change = (color) => {
+    const id = $classList("root");
+    id.remove("morado", "white", "black");
+    id.add(color);
+
+    localStorage.setItem("theme", color);
+  };
+
+  const local = () => {
+    try {
+      const color = localStorage.getItem("theme") || "default";
+
+      change(color);
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    local();
+    return () => {};
+  }, []);
   return (
     <>
-    <div
-      className="changeColor center-xy active"
-      id="changeColor"
-      style={position}
-      onClick={() => {
-        change();
-      }}
-    >
-      <Icono css="icon-color-lens center-xy" />
-      <nav>
-        <ul>
-          <li>
-            <button>
-              <span className="color morado"></span>Morado
-            </button>
-          </li>
-          <li>
-            <button>
-              <span className="color negro"></span>Negro
-            </button>
-          </li>
-          <li>
-            <button>
-              <span className="color blanco"></span>Blanco
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </div>
+      <div className="changeColor center-xy " id="changeColor" style={position}>
+        <span
+          className="cover"
+          onClick={() => {
+            active();
+          }}
+        ></span>
+        <Icono css="icon-color-lens center-xy" />
+        <nav>
+          <ul>
+            <li>
+              <button>
+                <Icono css="icon-color-lens center-xy" />
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  change("morado");
+                  active()
+                }}
+                id="morado"
+              >
+                <span className="color morado "></span>
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  change("black");
+                  active()
+                }}
+                id="negro"
+              >
+                <span className="color negro"></span>
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  change("white");
+                  active()
+                }}
+                id="blanco"
+              >
+                <span className="color blanco"></span>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 };
