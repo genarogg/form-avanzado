b2d872661af43225d4c48a0f0df8de4c7b3b4e8f
genaro gonzalez <genarrogg@gmail.com>
2023-06-29 10:39:57 -0400

    nuevas-dependencias

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Form from "./pages/my-form/components/form-login";
+import logo from './logo.svg';
+import './App.css';
 
-import "./css/icomoon/style.css";
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Form} />
-        <Route exact path="/form" component={Form} />
-        <Route component={Form} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
